ef2181c10691fb148b43fd18c8e7b307e0971c64
Fitria24 <72337273+Fitria24@users.noreply.github.com>
2020-11-22 08:13:09 -0800

    Add files via upload

---
+++ database/migrations/2020_11_21_041251_create_pesanans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePesanansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pesanans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_pemesan');
+            $table->string('alamat_pemesan');
+            $table->integer('notelpon_pemesan');
+            $table->string('barang_dipesan');
+            $table->integer('jumlah_dipesan');
+            $table->varchar('ukuran');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pesanans');
+    }
+}
